c02e3a0b9b09bd1d6e3f33c08902b86287ff1ae5
gkwjdtn975 <gkwjdtn975@naver.com>
2025-06-05 17:46:02 +0900

    feat(order): 주문 생성 API 구현
    - 회원/비회원 주문 요청 수신
    - order 및 order_item 테이블에 데이터 저장
    - wrapping 정보 처리 및 기본 '대기' 상태 설정
    
    Fixes #1

---
+++ src/main/java/com/nhnacademy/bookstoreorderapi/BookstoreOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookstoreorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreOrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreOrderApiApplication.class, args);
+    }
+
+}
