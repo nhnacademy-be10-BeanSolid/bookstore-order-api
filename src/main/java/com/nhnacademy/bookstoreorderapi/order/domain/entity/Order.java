dbfed6a575a4a1a1a4d4068f2df5e7792978c3d8
gkwjdtn975 <gkwjdtn975@naver.com>
2025-06-09 17:56:37 +0900

    feat(order): 주문 생성 API 구현
    - 회원/비회원 API
    - wrapping 정보 처리 및 기본 '대기' 상태 설정
    
    Fixes #1

---
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/domain/entity/Order.java
@@ -0,0 +1,52 @@
+package com.nhnacademy.bookstoreorderapi.order.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    @Column(name="guest_name")
+    private String guestName;
+
+    @Column(name="guest_phone")
+    private String guestPhone;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private LocalDateTime requestedAt;
+    private LocalDateTime deliveryAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private int totalPrice;
+    private int deliveryFee;
+    private int finalPrice;
+
+    /**
+     * @Builder로 생성할 때 초기화(default)가 무시되는 문제를 막기 위해
+     * @Builder.Default를 붙여줍니다.
+     */
+    @Builder.Default
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items = new ArrayList<>();
+
+    public void addItem(OrderItem item) {
+        item.setOrder(this);
+        this.items.add(item);
+    }
+}
---
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/domain/entity/OrderItem.java
@@ -0,0 +1,34 @@
+package com.nhnacademy.bookstoreorderapi.order.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Table(name = "order_item")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="order_id", insertable=false, updatable=false)
+    private Long orderId;
+
+    private Long bookId;
+
+    private int unitPrice;
+    private int quantity;
+
+    @Column(name="gift_wrapped")
+    private Boolean giftWrapped;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wrapping_id")
+    private Wrapping wrapping;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
