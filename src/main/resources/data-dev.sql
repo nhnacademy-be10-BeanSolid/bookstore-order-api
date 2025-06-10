45f1bbdceb81ea04d0b903ea157ff73fa17923ef
gkwjdtn975 <gkwjdtn975@naver.com>
2025-06-10 17:45:09 +0900

    chore(dev): H2 인메모리 wrapping 초기 데이터 추가
    - src/main/resources/data-dev.sql에 wrapping 샘플 레코드 삽입

---
+++ src/main/resources/data-dev.sql
@@ -0,0 +1,3 @@
+INSERT INTO wrapping(name, price, is_active) VALUES
+                                                 ('기본 포장', 2000, TRUE),
+                                                 ('선물 포장', 5000, TRUE);
