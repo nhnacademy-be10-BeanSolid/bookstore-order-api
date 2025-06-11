268d8e1bda3757d5c2d5bac0eaf79cbd5787f188
mni-js <2ujs4311@gmail.com>
2025-06-11 10:09:22 +0900

    Update OrderService.java
    import Collectors 추가

--- src/main/java/com/nhnacademy/bookstoreorderapi/order/service/OrderService.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/service/OrderService.java
@@ -18,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
