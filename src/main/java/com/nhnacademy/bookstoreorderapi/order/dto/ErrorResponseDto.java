90b8bec915b7d38dab295b24e84b2f156f41e554
gkwjdtn975 <gkwjdtn975@naver.com>
2025-06-12 09:44:36 +0900

    fix(dto): ErrorResponseDto 파일 추가 및 ResponseDto 인터페이스 적용 로직 보강
    Fixes #8

--- src/main/java/com/nhnacademy/bookstoreorderapi/order/dto/ErrorResponseDto.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/dto/ErrorResponseDto.java
@@ -1,11 +1,12 @@
+// src/main/java/com/nhnacademy/bookstoreorderapi/order/dto/ErrorResponseDto.java
 package com.nhnacademy.bookstoreorderapi.order.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.*;
 
-@Getter
+
+@Getter @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResponseDto implements ResponseDto {
-
-    private final String error;
+    private String error;
 }
