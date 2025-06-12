a0f914325e08fb228b204b558d7f58c016e5911b
gkwjdtn975 <gkwjdtn975@naver.com>
2025-06-12 09:44:43 +0900

    fix(entity): Order 엔티티 생성자 및 필드 검증 보완
    Fixes #8

--- src/main/java/com/nhnacademy/bookstoreorderapi/order/domain/entity/Order.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/domain/entity/Order.java
@@ -17,7 +17,7 @@ public class Order {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long userId;
+    private String userId;  //userId -> 타입 String 변경
 
     @Column(name="guest_name")
     private String guestName;
@@ -28,10 +28,10 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    private LocalDateTime requestedAt;
-    private LocalDateTime deliveryAt;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    private LocalDateTime requestedAt; // 반품 요청일
+    private LocalDateTime deliveryAt; // 배송 요청일
+    private LocalDateTime createdAt;   // 처음 저장된 시간
+    private LocalDateTime updatedAt;    //주문 정보(상태, 배송일) 마지막으로 수정된 시간
 
     private int totalPrice;
     private int deliveryFee;
