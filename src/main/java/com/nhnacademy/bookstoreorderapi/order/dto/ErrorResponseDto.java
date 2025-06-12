f13eb20ba7a11ea69928cd55b5a7b048f220ea97
hajungsoo <62366767+gkwjdtn975@users.noreply.github.com>
2025-06-12 10:18:39 +0900

    Update ErrorResponseDto.java
    ErrorResponseDto에 AllArgsConstructor를 붙여 생성자에서 error을 초기화 (추가)

--- src/main/java/com/nhnacademy/bookstoreorderapi/order/dto/ErrorResponseDto.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/dto/ErrorResponseDto.java
@@ -1,11 +1,11 @@
 // src/main/java/com/nhnacademy/bookstoreorderapi/order/dto/ErrorResponseDto.java
 package com.nhnacademy.bookstoreorderapi.order.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.*;
 
 
-@Getter @Setter
-@NoArgsConstructor
+@Getter
 @AllArgsConstructor
 public class ErrorResponseDto implements ResponseDto {
     private final String error;
