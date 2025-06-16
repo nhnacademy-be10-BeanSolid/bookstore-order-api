9b3c6b2716f14877281736fc820ab93d8de82d81
gkwjdtn975 <gkwjdtn975@naver.com>
2025-06-16 09:58:26 +0900

    feat(order): OrderResponseDto에 variable DEFAULT_DELIVERY_FEE 추가

--- src/main/java/com/nhnacademy/bookstoreorderapi/order/domain/entity/Order.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/domain/entity/Order.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class Order {
+    public static final int DEFAULT_DELIVERY_FEE = 5000;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_id")
