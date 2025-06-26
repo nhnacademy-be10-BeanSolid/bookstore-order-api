ba31b0d0166a4d6f28cd6c7323206c27aa88f572
gkwjdtn975 <gkwjdtn975@naver.com>
2025-06-26 16:58:44 +0900

    fix(payment): 결제 중복검사 enum 기반으로 수정

--- src/main/java/com/nhnacademy/bookstoreorderapi/order/repository/OrderRepository.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/repository/OrderRepository.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByUserId(Long userId);
-    boolean existsByOrderId(String orderId);
+//    boolean existsByOrderId(String orderId);
+    //Order 객체를 곧바로 넘겨야 해서 findByOrderId()로 선택함... 사실 existsByOrderId가 가벼워서 고민 많이함
 
+    /*OrderfindByOrderId()로 하면 뭐리 수를 1회로 최소화,
+    트랜잭션 내에서 영속 객체를 유지해 성능, 일관성을 챙김
+     */
 
     Order findByOrderId(String orderId);
 }
