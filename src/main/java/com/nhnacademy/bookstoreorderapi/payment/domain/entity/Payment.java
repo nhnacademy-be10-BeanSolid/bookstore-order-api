abd0bdfc771940392084b343cf53d9e513039721
gkwjdtn975 <gkwjdtn975@naver.com>
2025-06-26 17:21:12 +0900

    fix(payment): 결제 중복검사 enum 기반으로 수정

--- src/main/java/com/nhnacademy/bookstoreorderapi/payment/domain/entity/Payment.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/payment/domain/entity/Payment.java
@@ -21,7 +21,7 @@ public class Payment {
     private Long paymentId;
 
     //FK: 주문 번호
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "order_id", nullable = false)
     private Order order;
 
