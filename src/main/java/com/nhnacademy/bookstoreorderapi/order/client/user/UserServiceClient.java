99251d8295659576ba8dad59e49686a77ae38769
mni-js <2ujs4311@gmail.com>
2025-06-27 16:57:31 +0900

    chore: feignclient 이름 소문자로 변경, jpa create-drop -> update로 변경

--- src/main/java/com/nhnacademy/bookstoreorderapi/order/client/user/UserServiceClient.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/client/user/UserServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "USER-API")
+@FeignClient(name = "user-api")
 public interface UserServiceClient {
 
     @GetMapping("/users/me")
