1de7ae768411be3d42615762c0e27e9c3baaa5e7
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-01 15:26:55 +0900

    feat(payment): 샌드박스 전용 호출로 단순화
    - application-dev.yml 의 base-url 을 sandbox.tosspayments.com 으로 설정
    - Feign client 단일 호출만 사용하도록 기존 prod 우회 로직 제거
    - extractRedirectUrl 에서 checkout.url 우선 탐색 로직 추가

--- src/main/java/com/nhnacademy/bookstoreorderapi/payment/client/TossPaymentClient.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/payment/client/TossPaymentClient.java
@@ -1,30 +1,19 @@
-// src/main/java/com/nhnacademy/bookstoreorderapi/payment/client/TossPaymentClient.java
 package com.nhnacademy.bookstoreorderapi.payment.client;
 
-import com.nhnacademy.bookstoreorderapi.payment.config.TossFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
-
-@FeignClient(
-        name          = "toss",
-        url           = "${payment.toss.base-url}",
-        configuration = TossFeignConfig.class
-)
+@FeignClient(name="toss", url="${payment.toss.base-url}")
 public interface TossPaymentClient {
-
     @PostMapping("/payments")
-    Map<String, Object> createPayment(@RequestBody Map<String, Object> body);
+
+    Map<String,Object> createPayment(@RequestBody Map<String,Object> body);
 
     @PostMapping("/payments/{paymentKey}/confirm")
-    void confirmPayment(
-            @PathVariable String paymentKey,
-            @RequestBody    Map<String, Object> body
-    );
+    void confirmPayment(@PathVariable String paymentKey,
+                        @RequestBody Map<String,Object> body);
 
     @PostMapping("/payments/{paymentKey}/cancel")
-    Map<String, Object> cancelPayment(
-            @PathVariable String paymentKey,
-            @RequestBody    Map<String, Object> body
-    );
+    Map<String,Object> cancelPayment(@PathVariable String paymentKey,
+                                     @RequestBody Map<String,Object> body);
 }
