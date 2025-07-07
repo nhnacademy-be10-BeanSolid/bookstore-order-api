f743b3d63dae6bc37bdb90532c1975a4f96b1528
mni-js <2ujs4311@gmail.com>
2025-07-07 13:25:17 +0900

    chore: ErrorResponse를 전역처리클래스에 import

--- src/main/java/com/nhnacademy/bookstoreorderapi/order/common/advice/OrderExceptionHandler.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/common/advice/OrderExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.nhnacademy.bookstoreorderapi.order.common.dto.ErrorResponse;
+
 
 import java.util.HashMap;
 import java.util.Map;
