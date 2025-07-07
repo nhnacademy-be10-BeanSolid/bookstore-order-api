40d21423aafff950de9e56348f52f90bca915e7e
mni-js <2ujs4311@gmail.com>
2025-07-08 08:07:23 +0900

    fix: CustomOrderRepositoryImpl이 빈으로 등록되지 않던 문제 해결

--- src/main/java/com/nhnacademy/bookstoreorderapi/order/repository/impl/CustomOrderRepositoryImpl.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/repository/impl/CustomOrderRepositoryImpl.java
@@ -10,9 +10,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 @RequiredArgsConstructor
 public class CustomOrderRepositoryImpl implements CustomOrderRepository {
 
