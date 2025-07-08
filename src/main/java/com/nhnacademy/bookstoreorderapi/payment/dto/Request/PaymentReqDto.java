ab57fb4f5b31917d95fad9860e0522c326903e3f
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-08 18:54:49 +0900

    fix(payment):  PaymentReqDto.orderId 필드에 @JsonAlias({"order_id","orderId"}) 적용  · prod 환경(SNAKE_CASE 전략 활성)에서도 camelCase 요청을 허용

--- src/main/java/com/nhnacademy/bookstoreorderapi/payment/dto/Request/PaymentReqDto.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/payment/dto/Request/PaymentReqDto.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.bookstoreorderapi.payment.dto.Request;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.nhnacademy.bookstoreorderapi.payment.domain.PayType;
 import jakarta.validation.constraints.NotNull;
@@ -8,7 +9,7 @@ import lombok.Data;
 @Data
 
 public class PaymentReqDto {
-   @NotNull
+   @JsonAlias({"order_id","orderId"})
    private String orderId;
 
    @NotNull(message = "결제 금액(payAmount)는 필수입니다.")
