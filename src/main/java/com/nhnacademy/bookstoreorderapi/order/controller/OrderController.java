860f7176c9327b959479d5bab32d3d9c7c177b4e
mni-js <2ujs4311@gmail.com>
2025-07-15 17:08:12 +0900

    chore: OrderController 클래스의 메서드를 CRUD순서로 정렬

--- src/main/java/com/nhnacademy/bookstoreorderapi/order/controller/OrderController.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/controller/OrderController.java
@@ -1,18 +1,19 @@
 package com.nhnacademy.bookstoreorderapi.order.controller;
 
-import com.nhnacademy.bookstoreorderapi.order.common.resolver.XUserIdResolver;
 import com.nhnacademy.bookstoreorderapi.order.dto.request.CreateOrderRequest;
 import com.nhnacademy.bookstoreorderapi.order.dto.request.ReturnsRequest;
 import com.nhnacademy.bookstoreorderapi.order.dto.request.UpdateOrderRequest;
 import com.nhnacademy.bookstoreorderapi.order.dto.response.CreateOrderResponse;
 import com.nhnacademy.bookstoreorderapi.order.dto.response.OrderDetailResponse;
 import com.nhnacademy.bookstoreorderapi.order.dto.response.OrderResponse;
+import com.nhnacademy.bookstoreorderapi.order.dto.response.OrderSummaryResponse;
 import com.nhnacademy.bookstoreorderapi.order.exception.badrequest.InvalidRequestException;
-import com.nhnacademy.bookstoreorderapi.order.exception.unauthorized.NotMemberException;
 import com.nhnacademy.bookstoreorderapi.order.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,7 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    // CREATE (생성)
     // 주문 생성(회원, 비회원 둘 다 가능)
     @PostMapping
     public ResponseEntity<CreateOrderResponse> createOrder(@Valid @RequestBody CreateOrderRequest request,
@@ -32,31 +34,22 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(request, xUserId));
     }
 
-    //TODO: 일정 시간이 지났는데도 완료되지 않으면 주문 데이터를 삭제하게 구현할 예정
+    // READ (조회)
     // 주문서 작성 페이지
     @GetMapping("/{orderNumber}/input-detail")
-    public ResponseEntity<CreateOrderResponse> getUnfinishedOrder(@PathVariable String orderNumber,
+    public ResponseEntity<CreateOrderResponse> getUnfinishedOrder(@PathVariable String orderNumber, //TODO: 일정 시간이 지났는데도 완료되지 않으면 주문 데이터를 삭제하게 구현할 예정
                                                                   @RequestHeader(value = "X-USER-ID", required = false) String xUserId) {
         validateOrderNumber(orderNumber);
         return ResponseEntity.ok(orderService.getUnfinishedOrder(orderNumber, xUserId));
     }
 
-    // 주문 업데이트(포장 및 배송정보 업데이트)
-    @PutMapping("/{orderNumber}")
-    public ResponseEntity<OrderResponse> updateOrder(@PathVariable String orderNumber,
-                                                     @Valid @RequestBody UpdateOrderRequest request,
-                                                     @RequestHeader(value = "X-USER-ID", required = false) String xUserId) {
-        validateOrderNumber(orderNumber);
-        return ResponseEntity.ok(orderService.updateOrder(orderNumber, request, xUserId));
+    // 회원 주문 전체 조회
+    @GetMapping
+    public ResponseEntity<Page<OrderSummaryResponse>> getAllOrdersByUserId(@RequestHeader("X-USER-ID") String xUserId,
+                                                                           Pageable pageable) {
+        return ResponseEntity.ok().body(orderService.findAllByUserId(xUserId, pageable));
     }
-//
-//    // 회원 주문 전체 조회
-//    @GetMapping
-//    public ResponseEntity<Page<OrderSummaryResponse>> getAllOrdersByUserId(@RequestHeader("X-USER-ID") String xUserId,
-//                                                                           Pageable pageable) {
-//        return ResponseEntity.ok().body(orderService.findAllByUserId(xUserId, pageable));
-//    }
-//
+
     // 주문 상세 조회
     @GetMapping("/{orderNumber}")
     public ResponseEntity<OrderDetailResponse> getOrder(@PathVariable String orderNumber,
@@ -65,25 +58,24 @@ public class OrderController {
         return ResponseEntity.ok().body(orderService.findByOrderNumber(orderNumber, xUserId));
     }
 
-    // 반품 요청
+    // UPDATE (수정)
+    // 주문 업데이트(포장 및 배송정보 업데이트)
+    @PutMapping("/{orderNumber}")
+    public ResponseEntity<OrderResponse> updateOrder(@PathVariable String orderNumber,
+                                                     @Valid @RequestBody UpdateOrderRequest request,
+                                                     @RequestHeader(value = "X-USER-ID", required = false) String xUserId) {
+        validateOrderNumber(orderNumber);
+        return ResponseEntity.ok(orderService.updateOrder(orderNumber, request, xUserId));
+    }
+
+    // 반품 처리(주문 상태 변경)
     @PutMapping("/{orderNumber}/status")
     public ResponseEntity<OrderResponse> requestReturns(@PathVariable String orderNumber,
                                                         @Valid @RequestBody ReturnsRequest request,
                                                         @RequestHeader("X-USER-ID") String xUserId) {
         validateOrderNumber(orderNumber);
-
         return ResponseEntity.ok(orderService.changeStatusToReturned(orderNumber, request, xUserId));
     }
-//
-//    @PostMapping("/{orderId}/cancel")
-//    public SuccessResponseDto cancelOrder(
-//            @PathVariable String orderId,
-//            @RequestBody(required = false) CancelOrderRequestDto dto
-//    ) {
-//        String reason = (dto != null ? dto.getReason() : null);
-//        orderService.cancelOrder(orderId, reason);
-//        return new SuccessResponseDto("주문이 정상적으로 취소되었습니다.");
-//    }
 
     private void validateOrderNumber(String orderNumber) {
         if (orderNumber.isBlank()) {
