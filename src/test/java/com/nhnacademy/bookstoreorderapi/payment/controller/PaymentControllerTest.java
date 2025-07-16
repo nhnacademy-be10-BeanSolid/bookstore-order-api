9ad31ddca7a93b5b499ebb10e853761a640707d5
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-16 17:31:45 +0900

    feat: security 자동설정 제외

--- src/test/java/com/nhnacademy/bookstoreorderapi/payment/controller/PaymentControllerTest.java
+++ src/test/java/com/nhnacademy/bookstoreorderapi/payment/controller/PaymentControllerTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -25,9 +27,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(
-        value = PaymentController.class,
-        properties = "frontend.base-url=http://localhost"
+        controllers = PaymentController.class,
+        properties = "frontend.base-url=http://localhost",
+        excludeAutoConfiguration = SecurityAutoConfiguration.class
 )
+@AutoConfigureMockMvc(addFilters = false)
 class PaymentControllerTest {
 
     @Autowired
