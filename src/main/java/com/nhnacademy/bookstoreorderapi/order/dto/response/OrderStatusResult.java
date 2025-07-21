727fac1384e66b1ec455aa878ee2a8a5df75810b
mni-js <2ujs4311@gmail.com>
2025-07-21 15:49:14 +0900

    fix: 역직렬화 문제가 있어서 Jackson 라이브러리의 어노테이션으로 타입 정의 CancelResult, ReturnResult 중 어떤 구현체로 역직렬화할지 구분을 하지 못해 type 필드 추가해줌.

--- src/main/java/com/nhnacademy/bookstoreorderapi/order/dto/response/OrderStatusResult.java
+++ src/main/java/com/nhnacademy/bookstoreorderapi/order/dto/response/OrderStatusResult.java
@@ -1,7 +1,18 @@
 package com.nhnacademy.bookstoreorderapi.order.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.nhnacademy.bookstoreorderapi.payment.dto.Response.PaymentResDto;
 
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = OrderStatusResult.CancelResult.class, name = "cancel"),
+    @JsonSubTypes.Type(value = OrderStatusResult.ReturnResult.class, name = "return")
+})
 public sealed interface OrderStatusResult 
     permits OrderStatusResult.CancelResult, OrderStatusResult.ReturnResult {
     
